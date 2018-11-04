7c5c8dcc0f38d129fd6d23e570f8110014406b55
pablorcruh <pablorcruh@gmail.com>
2018-11-04 16:36:26 -0500

    finish main class and README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 [![CircleCI](https://circleci.com/gh/pablorcruh/pago-horas.svg?style=svg)](https://circleci.com/gh/pablorcruh/pago-horas)
-# pago-horas
+
+# PAGO-HORAS
+
+This project present a solution to a determine how much to pay to a person for his billable hours according to a
+the day of the week and the hour in the day
+
+The solution is staged in different packages, each package contains the foundantion for the solution.
+
+The package --constants-- contains global values used on the solution.
+
+The package --model-- contains classes to represent the workload in a day and the workinfo on the worker.
+
+The package --start-- constains the main class which uses all the logic to solve the problem.
+
+The package --service-- contains the interfaces which represent the business logic.
+
+The package --impl-- contains the implementation of the logic as the interface contract states.
+
+The solution uses Circle CI as integration server to run the test suite.
+
+Inside the project we have two text files that represent the input of the solution.
+
+The project can be executed from the terminal window using maven commands.
+
+We need to clean the project and download all the dependencies.
+
+```
+    mvn clean
+    
+    mvn install
+    
+    mvn exec:java -Dexec.mainClass="ec.com.ioet.start.Main" -Dexec.args="input1.txt"
+```
+
+
--- src/main/java/ec/com/ioet/start/Main.java
+++ src/main/java/ec/com/ioet/start/Main.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import ec.com.ioet.model.WorkInfo;
+import ec.com.ioet.model.Workload;
 import ec.com.ioet.services.ParseService;
 import ec.com.ioet.services.ReadInputService;
 import ec.com.ioet.services.impl.ParseServiceImpl;
@@ -15,17 +16,30 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// mvn exec:java -Dexec.mainClass="ec.com.ioet.start.Main" -Dexec.args="input.txt"
 		
+		int totalToPay = 0;
+		int totalHours = 0;
+		int auxCalculation =0;
+		int total =0;
 		ReadInputService readService=new ReadInputServiceImpl();
 		ParseService parseService = new ParseServiceImpl();
 		
 		String input = args[0];
 		BufferedReader bufferReader = new BufferedReader(new FileReader(input));
+		
 		WorkInfo workInfo = new WorkInfo();
 		
 		try {
-			String workInput = readService.readInput(bufferReader);
+			String workInput = readService.readInput(bufferReader);	
 			workInfo = parseService.parseMultipleInput(workInput);
-			
+			for(Workload dayWorkload: workInfo.getWorkload()) {
+				auxCalculation = 0;
+				totalHours = parseService.calculateHours(dayWorkload);
+				totalToPay = parseService.chooseTimeLine(dayWorkload);
+				auxCalculation = totalHours * totalToPay;
+				total = total + auxCalculation;
+			}
+			System.out.println("The amount to pay "+ workInfo.getName()+ " is: "+ total + " USD");
+				
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
