3cf1886ca4e15acf157a6910f23e2080f9a6e83c
pablorcruh <pablorcruh@gmail.com>
2018-11-04 15:52:54 -0500

    remove package utils

--- src/main/java/ec/com/ioet/start/Main.java
+++ src/main/java/ec/com/ioet/start/Main.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import ec.com.ioet.model.WorkInfo;
+import ec.com.ioet.services.ParseService;
 import ec.com.ioet.services.ReadInputService;
+import ec.com.ioet.services.impl.ParseServiceImpl;
 import ec.com.ioet.services.impl.ReadInputServiceImpl;
 
 public class Main {
@@ -12,14 +15,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// mvn exec:java -Dexec.mainClass="ec.com.ioet.start.Main" -Dexec.args="input.txt"
 		
-		ReadInputService service=new ReadInputServiceImpl();
+		ReadInputService readService=new ReadInputServiceImpl();
+		ParseService parseService = new ParseServiceImpl();
 		
 		String input = args[0];
 		BufferedReader bufferReader = new BufferedReader(new FileReader(input));
-
+		WorkInfo workInfo = new WorkInfo();
 		
 		try {
-			System.out.println(">>>>>>>>>>"+service.readInput(bufferReader));
+			String workInput = readService.readInput(bufferReader);
+			workInfo = parseService.parseMultipleInput(workInput);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/test/java/ec/com/ioet/PagosTest.java
+++ src/test/java/ec/com/ioet/PagosTest.java
@@ -1,14 +1,15 @@
 package ec.com.ioet;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
 import ec.com.ioet.model.WorkInfo;
 import ec.com.ioet.model.Workload;
-import ec.com.ioet.utils.Utils;
+import ec.com.ioet.services.ParseService;
+import ec.com.ioet.services.ReadInputService;
+import ec.com.ioet.services.impl.ParseServiceImpl;
+import ec.com.ioet.services.impl.ReadInputServiceImpl;
 
 public class PagosTest {
 
@@ -17,10 +18,13 @@ public class PagosTest {
 	String inputData1 = "RENE=MO10:00-12:00,TU10:00-12:00";
 	
     Workload workload =new Workload("SU","20:00","21:00");
+    
+	ReadInputService readService=new ReadInputServiceImpl();
+	ParseService parseService = new ParseServiceImpl();
 	
 	@Test
 	public void parseInput() {
-		Workload result = Utils.parse(inputData);
+		Workload result = parseService.parse(inputData);
 		assertEquals("MO", result.getDay());
 		assertEquals("10:00", result.getStartTime());
 		assertEquals("12:00", result.getStopTime());
@@ -29,20 +33,20 @@ public class PagosTest {
 	
 	@Test
 	public void parseMultipleInput() {
-		WorkInfo result = Utils.parseMultipleInput(inputData1);
+		WorkInfo result = parseService.parseMultipleInput(inputData1);
 		assertEquals(2,result.getWorkload().size());
 	}
 	
 	@Test
 	public void calculateHours( ) {
-		int result = Utils.calculateHours(workload);
+		int result = parseService.calculateHours(workload);
 		assertEquals(1,result);
 		
 	}
 	
 	@Test
 	public void chooseTimeLine() {
-		int result = Utils.chooseTimeLine(workload);
+		int result = parseService.chooseTimeLine(workload);
 		assertEquals(25, result);
 	}
 	
